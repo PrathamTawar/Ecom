86f832907ce757c51a2ecb0f48399bbc4770bfea
PrathamTawar <tawpra28@gmail.com>
2024-11-19 14:38:47 +0530

    added index to test deployment

--- app.js
+++ app.js
@@ -1,12 +1,6 @@
 async function getSite() {
     let data = await fetch('./frontend/Home/home.html')
     data = await data.text()
-
-    console.log(data.data)
-
     document.querySelector('html').innerHTML = data
-    console.log(document.querySelector('html'))
-    
 }
-
 getSite()
